f64ade7bdd10451dc89fcfccc5034b510f7dbe56
Akshat Nigam <akshat.dev.zero@gmail.com>
2021-04-12 01:01:05 +0530

    CreateAccount component created.

---
+++ src/features/auth/CreateAccount.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+
+// reactstrap components
+import { Button, Card, CardBody, Form, Row, Col } from 'reactstrap';
+
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+
+import { ErrorAlert, TextField } from 'components/atoms';
+import { mobileNumberValidator } from 'core/utils';
+import { useSignUpMutation } from 'generated/graphql';
+
+type FormValues = {
+  mobileNumber: string;
+};
+
+const schema = z.object({
+  mobileNumber: z.string().nonempty({ message: 'Please enter your mobile number!' }).refine(mobileNumberValidator, {
+    message: 'Please enter a valid mobile number!',
+  }),
+});
+
+const CreateAccount: React.FunctionComponent = ({ setMobileNumber, nextStage }: any) => {
+  const { mutate, isLoading, isError, error } = useSignUpMutation({
+    onSuccess: (data, variables) => {
+      if (data) {
+        setMobileNumber(variables.mobileNumber);
+        nextStage();
+      }
+    },
+  });
+  const { control, handleSubmit } = useForm({
+    resolver: zodResolver(schema),
+  });
+  const onSubmit = async (variables: FormValues) => mutate(variables);
+
+  return (
+    <>
+      <Card className="bg-secondary shadow border-0">
+        <CardBody className="px-lg-5 py-lg-5">
+          <div className="text-center text-muted mb-4">
+            <small>Sign up using mobile number</small>
+          </div>
+          <ErrorAlert isError={isError} error={error} />
+          <Form role="form" onSubmit={handleSubmit(onSubmit)}>
+            <TextField
+              name="mobileNumber"
+              type="tel"
+              labelType="prepend-icon"
+              labelValue="ni ni-mobile-button"
+              placeholder="Mobile Number"
+              autoComplete="username"
+              control={control}
+            />
+            <div className="text-center">
+              <Button className="my-2" color="primary" type="submit" disabled={isLoading}>
+                Sign up
+              </Button>
+            </div>
+          </Form>
+        </CardBody>
+      </Card>
+      <Row className="mt-3">
+        <Col xs="6">
+          <Link className="text-light" to="/auth/sign-in">
+            <small>Sign in instead</small>
+          </Link>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default CreateAccount;
--- src/views/SignIn.tsx
+++ src/views/SignIn.tsx
@@ -19,15 +19,16 @@ import React from 'react';
 
 // reactstrap components
 import { Button, Card, CardBody, Form, Row, Col } from 'reactstrap';
+
+import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 
-import TextField from 'components/atoms/TextField';
+import { ErrorAlert, TextField } from 'components/atoms';
 import { mobileNumberValidator } from 'core/utils';
 import { useSignInMutation } from 'generated/graphql';
 import { useAuthState } from 'features/auth';
-import ErrorAlert from 'components/atoms/ErrorAlert';
 
 type FormValues = {
   mobileNumber: string;
@@ -44,7 +45,7 @@ const schema = z.object({
 const Login: React.FunctionComponent = () => {
   const { signIn } = useAuthState();
   const { mutate, isLoading, isError, error } = useSignInMutation({
-    onSuccess: async (payload) => signIn(payload),
+    onSuccess: async (data) => signIn(data),
   });
   const { control, handleSubmit } = useForm({
     resolver: zodResolver(schema),
@@ -52,55 +53,53 @@ const Login: React.FunctionComponent = () => {
   const onSubmit = async (variables: FormValues) => mutate(variables);
 
   return (
-    <>
-      <Col lg="5" md="7">
-        <Card className="bg-secondary shadow border-0">
-          <CardBody className="px-lg-5 py-lg-5">
-            <div className="text-center text-muted mb-4">
-              <small>Sign in with credentials</small>
+    <Col lg="5" md="7">
+      <Card className="bg-secondary shadow border-0">
+        <CardBody className="px-lg-5 py-lg-5">
+          <div className="text-center text-muted mb-4">
+            <small>Sign in with credentials</small>
+          </div>
+          <ErrorAlert isError={isError} error={error} />
+          <Form role="form" onSubmit={handleSubmit(onSubmit)}>
+            <TextField
+              name="mobileNumber"
+              type="tel"
+              labelType="prepend-icon"
+              labelValue="ni ni-mobile-button"
+              placeholder="Mobile Number"
+              autoComplete="username"
+              control={control}
+            />
+            <TextField
+              name="password"
+              type="password"
+              labelType="prepend-icon"
+              labelValue="ni ni-lock-circle-open"
+              placeholder="Password"
+              autoComplete="current-password"
+              control={control}
+            />
+            <div className="text-center">
+              <Button className="my-2" color="primary" type="submit" disabled={isLoading}>
+                Sign in
+              </Button>
             </div>
-            <ErrorAlert isError={isError} error={error} />
-            <Form role="form" onSubmit={handleSubmit(onSubmit)}>
-              <TextField
-                name="mobileNumber"
-                type="tel"
-                labelType="prepend-icon"
-                labelValue="ni ni-mobile-button"
-                placeholder="Mobile Number"
-                autoComplete="username"
-                control={control}
-              />
-              <TextField
-                name="password"
-                type="password"
-                labelType="prepend-icon"
-                labelValue="ni ni-lock-circle-open"
-                placeholder="Password"
-                autoComplete="current-password"
-                control={control}
-              />
-              <div className="text-center">
-                <Button className="my-2" color="primary" type="submit" disabled={isLoading}>
-                  Sign in
-                </Button>
-              </div>
-            </Form>
-          </CardBody>
-        </Card>
-        <Row className="mt-3">
-          <Col xs="6">
-            <a className="text-light" href="#pablo" onClick={(e) => e.preventDefault()}>
-              <small>Forgot password?</small>
-            </a>
-          </Col>
-          <Col className="text-right" xs="6">
-            <a className="text-light" href="#pablo" onClick={(e) => e.preventDefault()}>
-              <small>Create new account</small>
-            </a>
-          </Col>
-        </Row>
-      </Col>
-    </>
+          </Form>
+        </CardBody>
+      </Card>
+      <Row className="mt-3">
+        <Col xs="6">
+          <a className="text-light" href="#pablo" onClick={(e) => e.preventDefault()}>
+            <small>Forgot password?</small>
+          </a>
+        </Col>
+        <Col className="text-right" xs="6">
+          <Link className="text-light" to="/auth/sign-up">
+            <small>Create new account</small>
+          </Link>
+        </Col>
+      </Row>
+    </Col>
   );
 };
 
