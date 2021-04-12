f226a06ed5f409f0cd0076c14fce1991d7968365
Akshat Nigam <akshat.dev.zero@gmail.com>
2021-04-12 17:13:07 +0530

    on the basis of current signed in user role type the layout is switched.

--- src/app.tsx
+++ src/app.tsx
@@ -18,7 +18,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-import { Layout, AuthLayout, AdminLayout } from 'layouts';
+import { Layout, AuthLayout, AdminLayout, CompanyOwnerLayout } from 'layouts';
 import { useAuthState } from 'features/auth';
 
 const AuthSwitch = (
@@ -35,12 +35,32 @@ const AdminSwitch = (
   </Switch>
 );
 
+const CompanyOwnerSwitch = (
+  <Switch>
+    <Route path={Layout.CompanyOwner} render={(props) => <CompanyOwnerLayout {...props} />} />
+    <Redirect from="/" to={Layout.CompanyOwner + '/index'} />
+  </Switch>
+);
+
 const App: React.FunctionComponent = () => {
-  const { isSignedIn } = useAuthState();
+  const { isSignedIn, user } = useAuthState();
 
   let sw = AuthSwitch;
   if (isSignedIn) {
-    sw = AdminSwitch;
+    if (user?.role?.type) {
+      switch (user.role.type) {
+        case 'platform_master_admin':
+          sw = AdminSwitch;
+          break;
+
+        case 'company_owner':
+          sw = CompanyOwnerSwitch;
+          break;
+
+        default:
+          break;
+      }
+    }
   }
 
   return <BrowserRouter>{sw}</BrowserRouter>;
