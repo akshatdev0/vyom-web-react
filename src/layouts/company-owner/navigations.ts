082ac1ae23c4e5fa65de2fd7ccf977ab5c89f984
Exilien Supreme <exilien.solutions@gmail.com>
2021-06-08 19:58:43 +0530

    Sidebar cleaned from demo items.

--- src/layouts/company-owner/navigations.ts
+++ src/layouts/company-owner/navigations.ts
@@ -16,15 +16,7 @@
 
 */
 import { Navigation } from 'core/navigation';
-import Components from 'views/pro/pages/forms/Components.js';
 import Dashboard from 'views/pro/pages/dashboards/Dashboard.js';
-import Elements from 'views/pro/pages/forms/Elements.js';
-import Google from 'views/pro/pages/maps/Google.js';
-import ReactBSTables from 'views/pro/pages/tables/ReactBSTables.js';
-import Sortable from 'views/pro/pages/tables/Sortable.js';
-import Tables from 'views/pro/pages/tables/Tables.js';
-import Validation from 'views/pro/pages/forms/Validation.js';
-import Vector from 'views/pro/pages/maps/Vector.js';
 
 import { CompanyOwnerProfile } from 'features/company-owner';
 import { Company } from 'features/company';
@@ -68,86 +60,6 @@ const sidebarMenu: Navigation = [
     icon: 'ni ni-align-left-2 text-default',
     path: '/orders',
     component: Orders,
-  },
-  {
-    identifier: 'tables',
-    name: 'Tables',
-    icon: 'ni ni-align-left-2 text-default',
-    collapse: true,
-    children: [
-      {
-        identifier: 'basic-tables',
-        path: '/basic-tables',
-        name: 'Basic Tables',
-        initial: 'BT',
-        component: Tables,
-      },
-      {
-        identifier: 'sortable',
-        path: '/sortable',
-        name: 'Sortable',
-        initial: 'S',
-        component: Sortable,
-      },
-      {
-        identifier: 'react-bs-table',
-        path: '/react-bs-table',
-        name: 'React BS Tables',
-        initial: 'RBT',
-        component: ReactBSTables,
-      },
-    ],
-  },
-  {
-    identifier: 'forms',
-    name: 'Forms',
-    icon: 'ni ni-single-copy-04 text-pink',
-    collapse: true,
-    children: [
-      {
-        identifier: 'elements',
-        path: '/elements',
-        name: 'Elements',
-        initial: 'E',
-        component: Elements,
-      },
-      {
-        identifier: 'components',
-        path: '/components',
-        name: 'Components',
-        initial: 'C',
-        component: Components,
-      },
-      {
-        identifier: 'validation',
-        path: '/validation',
-        name: 'Validation',
-        initial: 'V',
-        component: Validation,
-      },
-    ],
-  },
-  {
-    identifier: 'maps',
-    collapse: true,
-    name: 'Maps',
-    icon: 'ni ni-map-big text-primary',
-    children: [
-      {
-        identifier: 'google',
-        path: '/google',
-        name: 'Google',
-        initial: 'G',
-        component: Google,
-      },
-      {
-        identifier: 'vector',
-        path: '/vector',
-        name: 'Vector',
-        initial: 'V',
-        component: Vector,
-      },
-    ],
   },
 ];
 
