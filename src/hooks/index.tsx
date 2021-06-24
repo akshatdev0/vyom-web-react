7001a59d4993824838a08777afb82ad068d5a5a9
Exilien Supreme <exilien.solutions@gmail.com>
2021-06-24 19:21:05 +0530

    RouteDebugger hook defined and used in app.tsx

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,3 +1,4 @@
 export { default as useFillForm } from './useFillForm';
+export { default as useRouteDebugger } from './useRouteDebugger';
 export { default as useScrollTop } from './useScrollTop';
 export { default as useToggleSidebar } from './useToggleSidebar';
