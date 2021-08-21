669f173f9c66fb3fa910018d6c1e51baea0dc4d8
Akshat Nigam <akshat.dev.zero@gmail.com>
2021-08-21 15:30:46 +0530

    NewProduct BasicDetailsForm completed.

--- src/features/new-product/NewProduct.tsx
+++ src/features/new-product/NewProduct.tsx
@@ -1,26 +1,95 @@
 import React from 'react';
 
+import Accordion from '@material-ui/core/Accordion';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
 import Box from '@material-ui/core/Box';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import componentStyles from 'assets/theme/views/admin/container';
 import { ViewHeader } from 'components/molecules';
 
+import BasicDetailsForm from './BasicDetailsForm';
+
 const useStyles = makeStyles(componentStyles);
+const useLocalStyles = makeStyles(() =>
+  createStyles({
+    accordionDetailRoot: {
+      display: 'block',
+      flex: '1 1 auto',
+      padding: '1.5rem',
+    },
+  }),
+);
 
 const NewProduct: React.FunctionComponent = () => {
-  const classes = useStyles();
+  const classes = { ...useStyles(), ...useLocalStyles() };
 
   return (
     <>
       <ViewHeader title="Add a New Product" breadcrumbs={['Product Catalog', 'New Product']} />
       <Container maxWidth={false} classes={{ root: classes.containerRoot }}>
-        <Paper classes={{ root: classes.paperRoot }} elevation={1}>
-          <Grid container component={Box} height="100%"></Grid>
-        </Paper>
+        <Card classes={{ root: classes.cardRootLightGray }} elevation={1}>
+          <CardContent component={Box} height="100%">
+            <Accordion defaultExpanded>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+                <Typography>1. Fill Basic Details</Typography>
+              </AccordionSummary>
+              <AccordionDetails className={classes.accordionDetailRoot}>
+                <BasicDetailsForm />
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+                <Typography>2. Inventory and Pricing</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet
+                  blandit leo lobortis eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+                <Typography>3. Product Images</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet
+                  blandit leo lobortis eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+                <Typography>4. Specifications &amp; Variant &amp; Tags</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet
+                  blandit leo lobortis eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+                <Typography>5. Product Images</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet
+                  blandit leo lobortis eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </CardContent>
+        </Card>
       </Container>
     </>
   );
